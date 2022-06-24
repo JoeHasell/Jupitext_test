8aff48da6c210852a2a99a36a3f3621a3dea8ea7
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-06-24 22:21:13 +0100

    Added example R script and gitignore of notebooks

---
+++ R_scripts/Test_R_code.R
@@ -0,0 +1,38 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,Rmd,R:percent
+#     text_representation:
+#       extension: .R
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# %%
+library(tidyverse)
+
+# %% [markdown]
+# Pulling in Michalis PIP data
+
+# %%
+url<- "https://raw.githubusercontent.com/owid/notebooks/main/MichalisMoatsos/MainDataCountriesOnlyNew.csv"
+
+df <- read.csv(url, row.names = 1, header = T, sep = ";", dec = ",")
+
+
+# %%
+head(df)
+
+# %% [markdown]
+# This is a chart of Angola, $1.90 a day headcount ratio.
+
+# %%
+
+df %>% 
+    filter(Entity == "AGO") %>%
+    ggplot(aes(x=Year, y=headcount_ratio_1_90)) + geom_line()
